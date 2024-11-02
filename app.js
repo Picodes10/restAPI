aebfed8908fe3b9bb201a6008698ac634d229916
Sushree Manaswini Biswal <biswalsushree000@gmail.com>
2024-11-02 23:53:54 +0530

    First Commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require(‘express’);
 const app = express ();
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
     console.log("Server Listening on PORT:", port);
